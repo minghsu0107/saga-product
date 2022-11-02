67073d1d6d41619a57841bbb4651b846ed4de1af
Hao-Ming, Hsu <50090692+minghsu0107@users.noreply.github.com>
2022-11-02 13:35:54 +0800

    Update time.go

--- pkg/time.go
+++ pkg/time.go
@@ -8,7 +8,7 @@ import (
 
 // Time2pbTimestamp converter
 func Time2pbTimestamp(now time.Time) *timestamp.Timestamp {
-	s := int64(now.Second())
+	s := int64(now.Unix())
 	n := int32(now.Nanosecond())
 	return &timestamp.Timestamp{Seconds: s, Nanos: n}
 }
